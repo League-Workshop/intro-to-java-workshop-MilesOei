a4899a1f26c57a26288780ffbb8d0d6a701982ab
CactusCat <wjmorris2004@gmail.com>
2017-08-01 11:52:57 -0700

    2nd Day
    Intro to Java Workshop

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,11 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2 = new Robot();
+r2d2.setSpeed(9);
+r2d2.sparkle();
 
-	
-		
-		
+
+}
 	}
 }
