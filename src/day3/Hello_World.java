7aceff21c97f09cbe9e9b5c7d4136bf787da3716
CactusCat <wjmorris2004@gmail.com>
2017-08-02 11:56:01 -0700

    Day 3

---
+++ src/day3/Hello_World.java
@@ -0,0 +1,18 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class Hello_World {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		String name = JOptionPane.showInputDialog("Enter Your Name");
+		JOptionPane.showMessageDialog(null, "Good Morning " + name);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/day3/QuizGame.java
@@ -0,0 +1,63 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class QuizGame {
+	public static void main(String[] args) {
+		int score = 0;
+		
+		JOptionPane.showMessageDialog(null, "Welcome to my quiz game! \nAnswer the questions correctly and you get a point.");
+		
+		String first = JOptionPane.showInputDialog("What is 13+15?");
+		
+		if (first.equals("28")) {
+			JOptionPane.showMessageDialog(null, "You are correct! \nNext Question...");			
+		score = score + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. \nNext Question...");
+		}
+
+		
+		String second = JOptionPane.showInputDialog("Who was the first president?");
+		
+		if (second.equalsIgnoreCase("George Washington")) {
+			JOptionPane.showMessageDialog(null, "You are correct! \nNext Question...");			
+		score = score + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. \nNext Question...");
+		}
+
+			
+		String third = JOptionPane.showInputDialog("What question is this?");
+		
+		if (third.equals("3")) {
+			JOptionPane.showMessageDialog(null, "You are correct! \nNext Question...");			
+		score = score + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. \nNext Question...");
+		}
+
+		
+	String fourth = JOptionPane.showInputDialog("How do you say Hello in Spanish?");
+		
+		if (fourth.equalsIgnoreCase("Hola")) {
+			JOptionPane.showMessageDialog(null, "You are correct! \nNext Question...");			
+		score = score + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. \nNext Question...");
+		}
+
+		
+	String fifth = JOptionPane.showInputDialog("Fill in the blank \nNeil Armstrong was the first man on the ____.");
+		
+		if (fifth.equalsIgnoreCase("Moon")) {
+			JOptionPane.showMessageDialog(null, "You are correct! \n\n Your score is " + score + "\nThanks for playing!");			
+		score = score + 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "You are wrong. \nNext Question...");
+		}
+
+		
+		
+	}
+}
